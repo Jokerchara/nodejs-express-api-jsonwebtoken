9aa3241fdb156587943a351fde155fa14f830583
Al bi <albilydp4ever@gmail.com>
2022-06-13 19:28:33 +0700

    reload

--- src/configs/database.js
+++ src/configs/database.js
@@ -1,8 +1,8 @@
 require('dotenv').config();
 module.exports = {
-    multipleStatements  : true,
-    host                : "sql6.freemysqlhosting.net",
-    user                : "sql6498920",
-    password            : "atwEmgTqwj",
-    database            : "sql6498920"
+    multipleStatements: true,
+    host: "sql6.freemysqlhosting.net",
+    user: "sql6499478",
+    password: "UWVy1dtCY7",
+    database: "sql6499478"
 };
